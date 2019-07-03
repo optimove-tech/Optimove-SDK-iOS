1fbdd14f5c0f193d11a61bbe7aa951b520939981
Noy Grisaru <noy_g@skoobi.io>
2019-07-03 18:50:15 +0300

    Updated Swift demo app

--- DemoApplication/SwiftDemoApp/SwiftDemoApp/ViewController.swift
+++ DemoApplication/SwiftDemoApp/SwiftDemoApp/ViewController.swift
@@ -2,29 +2,29 @@
 import UIKit
 import OptimoveSDK
 
-class ViewController: UIViewController, OptimoveDeepLinkCallback {
-    func didReceive(deepLink: OptimoveDeepLinkComponents?)
-    {
-        guard let deepLink = deepLink else {return}
-        guard let vc = storyboard?.instantiateViewController(withIdentifier: "deepLinkVc") as? DeepLinkViewController else { return }
-
-        vc.deepLinkComp = deepLink
-       
-        present(vc, animated: true)
-    }
+class ViewController: UIViewController, OptimoveSuccessStateListener, OptimoveDeepLinkCallback {
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
         Optimove.shared.register(deepLinkResponder: OptimoveDeepLinkResponder(self))
+        Optimove.shared.registerSuccessStateListener(self)
     }
     
-    override func viewDidAppear(_ animated: Bool)
-    {
-        super.viewDidAppear(animated)
-        Optimove.shared.setScreenVisit(screenPathArray: ["main screen"], screenTitle: "main screen")
+    func optimove(_ optimove: Optimove, didBecomeActiveWithMissingPermissions missingPermissions: [OptimoveDeviceRequirement]) {
+        // Report this
+        Optimove.shared.setScreenVisit(
+            screenPath: "Home/Store/Footware/Boots",
+            screenTitle: "<YOUR_TITLE>",
+            screenCategory: "<OPTIONAL: YOUR_CATEGORY>"
+        )
+        // OR that
+        Optimove.shared.setScreenVisit(
+            screenPathArray: ["Home", "Store", "Footware", "Boots"],
+            screenTitle: "<YOUR_TITLE>",
+            screenCategory: "<OPTIONAL: YOUR_CATEGORY>"
+        )
     }
-    
     
     @IBAction func subscribrToTest(_ sender: UIButton)
     {
@@ -35,7 +35,14 @@ class ViewController: UIViewController, OptimoveDeepLinkCallback {
     {
         Optimove.shared.stopTestMode()
     }
-    
+
+    func didReceive(deepLink: OptimoveDeepLinkComponents?)
+    {
+        guard let deepLink = deepLink else {return}
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: "deepLinkVc") as? DeepLinkViewController else { return }
+        
+        vc.deepLinkComp = deepLink
+        
+        present(vc, animated: true)
+    }
 }
-
-
