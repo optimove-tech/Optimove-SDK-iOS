11c7b00212c318715f96cbd3b37c09341571e2f8
Eli Gutovsky <eli_g@optimove.com>
2019-09-08 18:44:26 +0300

    [removing-state-listener] Extract Deeplink service to a file

--- OptimoveSDK/Classes/Optimove/API/Optimove.swift
+++ OptimoveSDK/Classes/Optimove/API/Optimove.swift
@@ -51,39 +51,6 @@ import OptimoveCore
 
 }
 
-final class DeeplinkService {
-
-    private var deepLinkResponders = [OptimoveDeepLinkResponder]()
-    private var deepLinkComponents: OptimoveDeepLinkComponents? = nil
-
-    func setDeepLinkComponents(_ component: OptimoveDeepLinkComponents) {
-        self.deepLinkComponents = component
-        deepLinkResponders.forEach { responder in
-            responder.didReceive(deepLinkComponent: component)
-        }
-    }
-
-}
-
-extension DeeplinkService: OptimoveDeepLinkResponding {
-
-    func register(deepLinkResponder responder: OptimoveDeepLinkResponder) {
-        if let deepLinkComponents = deepLinkComponents {
-            responder.didReceive(deepLinkComponent: deepLinkComponents)
-        } else {
-            deepLinkResponders.append(responder)
-        }
-    }
-
-    func unregister(deepLinkResponder responder: OptimoveDeepLinkResponder) {
-        if let index = self.deepLinkResponders.firstIndex(of: responder) {
-            deepLinkResponders.remove(at: index)
-        }
-    }
-
-
-}
-
 // MARK: - Initialization API
 
 extension Optimove {
