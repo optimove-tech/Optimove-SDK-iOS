49de3fbb54ba4e68b484de34c36c09faf3bc7b7f
Eli Gutovsky <eli_g@optimove.com>
2019-09-19 21:56:01 +0300

    [rc-2.3.0] Use SWIFT_ACTIVE_COMPILATION_CONDITIONS

--- OptimoveCore/Classes/Constants/SDK.swift
+++ OptimoveCore/Classes/Constants/SDK.swift
@@ -4,43 +4,16 @@ import Foundation
 
 public struct SDK {
 
-    private struct Constants {
-        struct Bundle {
-            static let identifier = "com.optimove.sdk"
-        }
-        struct Key {
-            static let staging = "OPTIMOVE_CLIENT_STG_ENV"
-            static let environment = "OPTIMOVE_SDK_ENVIRONMENT"
-        }
-        struct Var {
-            static let `true` = "true"
-            static let `false` = "false"
-        }
-    }
-
     public static var environment: Environment {
-        guard let envvar = getEnvironmentVariable(for: Constants.Key.environment),
-              let value = Environment(rawValue: envvar)
-        else {
-            return .prod
-        }
-        return value
+        return isStaging ? .dev : .prod
     }
 
     public static var isStaging: Bool {
-        let envvar = getEnvironmentVariable(for: Constants.Key.staging, defaultValue: Constants.Var.false)
-        return envvar == Constants.Var.true
+        #if DEBUG
+            return true
+        #else
+            return false
+        #endif
     }
 
-    static func getEnvironmentVariable(for key: String, defaultValue: String) -> String {
-        guard let envVarValue = getEnvironmentVariable(for: key), !envVarValue.isEmpty else {
-            return defaultValue
-        }
-        return envVarValue
-    }
-
-    static func getEnvironmentVariable(for key: String) -> String? {
-        let envvarValue = Bundle.main.object(forInfoDictionaryKey: key) as? String
-        return envvarValue?.trimmingCharacters(in: .whitespacesAndNewlines)
-    }
 }
