d300f9687d4deefe1a56ea03eed4f320a1f24ee7
Eli Gutovsky <eli_g@optimove.com>
2019-09-20 21:27:40 +0300

    [rc-2.3.0] Revert "[rc-2.3.0] Use SWIFT_ACTIVE_COMPILATION_CONDITIONS"
    This reverts commit 49de3fbb54ba4e68b484de34c36c09faf3bc7b7f.

--- OptimoveCore/Classes/Constants/SDK.swift
+++ OptimoveCore/Classes/Constants/SDK.swift
@@ -4,16 +4,43 @@ import Foundation
 
 public struct SDK {
 
+    private struct Constants {
+        struct Bundle {
+            static let identifier = "com.optimove.sdk"
+        }
+        struct Key {
+            static let staging = "OPTIMOVE_CLIENT_STG_ENV"
+            static let environment = "OPTIMOVE_SDK_ENVIRONMENT"
+        }
+        struct Var {
+            static let `true` = "true"
+            static let `false` = "false"
+        }
+    }
+
     public static var environment: Environment {
-        return isStaging ? .dev : .prod
+        guard let envvar = getEnvironmentVariable(for: Constants.Key.environment),
+              let value = Environment(rawValue: envvar)
+        else {
+            return .prod
+        }
+        return value
     }
 
     public static var isStaging: Bool {
-        #if DEBUG
-            return true
-        #else
-            return false
-        #endif
+        let envvar = getEnvironmentVariable(for: Constants.Key.staging, defaultValue: Constants.Var.false)
+        return envvar == Constants.Var.true
     }
 
+    static func getEnvironmentVariable(for key: String, defaultValue: String) -> String {
+        guard let envVarValue = getEnvironmentVariable(for: key), !envVarValue.isEmpty else {
+            return defaultValue
+        }
+        return envVarValue
+    }
+
+    static func getEnvironmentVariable(for key: String) -> String? {
+        let envvarValue = Bundle.main.object(forInfoDictionaryKey: key) as? String
+        return envvarValue?.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
 }
--- OptimoveCore/Classes/Logger/LoggerSettings.swift
+++ OptimoveCore/Classes/Logger/LoggerSettings.swift
@@ -1,10 +1,26 @@
 //  Copyright © 2019 Optimove. All rights reserved.
 
+import Foundation
+
 public struct LoggerSettings {
 
+    private struct Constants {
+        struct Key {
+            static let logLevel = "OPTIMOVE_MIN_LOG_LEVEL"
+        }
+    }
+
     public static var logLevelToShow: LogLevelCore = {
+        if let minLogLevel = logLevel {
+            return minLogLevel
+        }
         return SDK.isStaging ? LogLevelCore.info : LogLevelCore.warn
     }()
+
+    private static var logLevel: LogLevelCore? {
+        guard let levelStr = SDK.getEnvironmentVariable(for: Constants.Key.logLevel) else { return nil }
+        return LogLevelCore(string: levelStr.lowercased())
+    }
 
 }
 
