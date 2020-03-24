01160be54fd7130c8d1f32fe62b66b16b9533a92
Eli Gutovsky <eli_g@optimove.com>
2020-03-24 12:11:05 +0200

    [updated-meta-event] switch to a global config v3

--- OptimoveCore/Sources/Classes/Constants/Endpoint.swift
+++ OptimoveCore/Sources/Classes/Constants/Endpoint.swift
@@ -19,7 +19,7 @@ public struct Endpoints {
         public struct GlobalConfig {
             public static var url = URL(string: "https://sdk-cdn.optimove.net/configs/mobile")!
                 .appendingPathComponent("global")
-                .appendingPathComponent("v2")
+                .appendingPathComponent("v3")
                 .appendingPathComponent(SdkEnvironment.getBuildSetting(for: "OPTIMOVE_CONFIG_ENV_PATH", defaultValue: "prod"))
                 .appendingPathComponent("configs.json")
         }
