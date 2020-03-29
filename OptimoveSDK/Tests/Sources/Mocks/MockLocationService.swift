8c2f0e73062eceb8ac28a09eff857c15a200be9a
Eli Gutovsky <eli_g@optimove.com>
2020-03-29 13:37:18 +0300

    [dev] fix test

--- OptimoveSDK/Tests/Sources/Mocks/MockLocationService.swift
+++ OptimoveSDK/Tests/Sources/Mocks/MockLocationService.swift
@@ -1,9 +1,12 @@
 //  Copyright © 2020 Optimove. All rights reserved.
 
 import Foundation
+import CoreLocation
 @testable import OptimoveSDK
 
 final class MockLocationService: LocationService {
+
+    func useLocationManager(_ locationManager: CLLocationManager) {}
 
     func getLocation(onComplete: @escaping (Result<[Location: String], LocationError>) -> Void) {
         onComplete(.success([
