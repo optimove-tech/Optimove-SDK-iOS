f2edbf8f12afee9c0a6944bfae66164b5ac5b97a
Eli Gutovsky <eli_g@optimove.com>
2020-04-23 11:33:23 +0300

    [optistream] Convert event context to json

--- OptimoveSDK/Sources/Classes/Components/OptiTrack/Optistream/OptistreamEventBuilder.swift
+++ OptimoveSDK/Sources/Classes/Components/OptiTrack/Optistream/OptistreamEventBuilder.swift
@@ -29,7 +29,7 @@ final class OptistreamEventBuilder {
             customer: storage.customerID,
             visitor: try storage.getVisitorID(),
             timestamp: event.timestamp,
-            context: event.context
+            context: try JSON(event.context)
         )
     }
 
