d16dd9313b4b339608e41b879043b95994378021
Eli Gutovsky <eli_g@optimove.com>
2021-03-11 13:00:28 +0200

    [dev] use channel set as array for UserDefaults

--- OptimoveCore/Sources/Classes/Extension/Set+map.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2021 Optimove. All rights reserved.
-
-public extension Set {
-
-    func map<U>(transform: (Element) -> U) -> Set<U> {
-        return Set<U>(self.lazy.map(transform))
-    }
-
-}
--- OptimoveCore/Sources/Classes/Storage/StorageFacade.swift
+++ OptimoveCore/Sources/Classes/Storage/StorageFacade.swift
@@ -65,7 +65,7 @@ public protocol StorageValue {
     /// Legacy. Use `firstRunTimestamp` instead
     var firstVisitTimestamp: Int64? { get set }
     /// Store user's allowed push notification channels.
-    var pushNotificationChannels: Set<String>? { get set }
+    var pushNotificationChannels: [String]? { get set }
 
     func getConfigurationEndPoint() throws -> URL
     func getCustomerID() throws -> String
@@ -349,7 +349,7 @@ public extension KeyValueStorage where Self: StorageValue {
         }
     }
 
-    var pushNotificationChannels: Set<String>? {
+    var pushNotificationChannels: [String]? {
         get {
             return self[.pushNotificationChannels]
         }
