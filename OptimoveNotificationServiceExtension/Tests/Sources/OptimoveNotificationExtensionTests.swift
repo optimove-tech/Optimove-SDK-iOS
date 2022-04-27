d1f509b1b747abb71e3e6279b05d60a7d4cc23ff
Christopher Wyllie <c.wyllie@kumulos.com>
2022-04-27 17:04:37 +0100

    Rename OptimoveNotificationExtension to OptimoveNotificationServiceExtension as it was

--- OptimoveNotificationServiceExtension/Tests/Sources/OptimoveNotificationExtensionTests.swift
+++ OptimoveNotificationServiceExtension/Tests/Sources/OptimoveNotificationExtensionTests.swift
@@ -6,7 +6,7 @@
 //
 
 import XCTest
-@testable import OptimoveNotificationExtension
+@testable import OptimoveNotificationServiceExtension
 
 class OptimoveNotificationExtensionTests: XCTestCase {
 
