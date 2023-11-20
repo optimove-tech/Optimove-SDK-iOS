109a81147523a9d719453422f44fea11b8ff5fe9
Eli Gutovsky <eli_g@optimove.com>
2023-11-21 00:23:52 +0200

    chore: bootstrap

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -3,6 +3,16 @@
 # Stop the script if any command fails
 set -e -o pipefail
 
+# Print the info
+echo "----------------------------------------------------------------------------------------------------"
+echo "Root: $ROOT"
+echo "Project: $PROJECT"
+echo "Scheme: $SCHEME"
+echo "Configuration: $CONFIGURATION"
+echo "SDK: $SDK"
+echo "Destination: $DESTANATION"
+echo "----------------------------------------------------------------------------------------------------"
+
 echo "Checking dependencies..."
 
 # Check if xcodebuild is installed
@@ -11,33 +21,22 @@ if ! command -v xcodebuild &>/dev/null; then
     exit 1
 fi
 
+# Check if Homebrew is installed
+if ! command -v brew &>/dev/null; then
+    echo "Error: Homebrew is not installed. Cannot install XcodeGen."
+    exit 1
+fi
+
 # Check if XcodeGen is installed, if not, attempt to install it
 if ! command -v xcodegen &>/dev/null; then
     echo "XcodeGen is not installed. Attempting to install using Homebrew..."
-    if ! command -v brew &>/dev/null; then
-        echo "Error: Homebrew is not installed. Cannot install XcodeGen."
-        exit 1
-    fi
     brew install xcodegen
 fi
 
 # Check if xcbeautify is installed, if not, attempt to install it
 if ! command -v xcbeautify &>/dev/null; then
     echo "xcbeautify is not installed. Attempting to install using Homebrew..."
-    if ! command -v brew &>/dev/null; then
-        echo "Error: Homebrew is not installed. Cannot install xcbeautify."
-        exit 1
-    fi
     brew install xcbeautify
 fi
 
 echo "Dependencies are installed."
-
-# Print the info
-echo "----------------------------------------------------------------------------------------------------"
-echo "Project: $PROJECT"
-echo "Scheme: $SCHEME"
-echo "Configuration: $CONFIGURATION"
-echo "SDK: $SDK"
-echo "Destination: $DESTANATION"
-echo "----------------------------------------------------------------------------------------------------"
