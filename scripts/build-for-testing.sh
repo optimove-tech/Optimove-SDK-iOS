052c9509313edf635dff281ccdbdc3c232427f90
Eli Gutovsky <eli_g@optimove.com>
2023-11-28 21:59:19 +0200

    build: fix ci

--- scripts/build-for-testing.sh
+++ scripts/build-for-testing.sh
@@ -9,14 +9,14 @@ source $ROOT/configurations/test.xcconfig
 # Bootstrap the project
 source $ROOT/scripts/bootstrap.sh
 
-# Run XcodeGen to regenerate the project
-echo "Removing existing Xcode project..."
-rm -rf "$PROJECT"
+# # Run XcodeGen to regenerate the project
+# echo "Removing existing Xcode project..."
+# rm -rf "$PROJECT"
 
-echo "Generating new Xcode project with XcodeGen..."
-xcodegen
+# echo "Generating new Xcode project with XcodeGen..."
+# xcodegen
 
-echo "----------------------------------------------------------------------------------------------------"
+# echo "----------------------------------------------------------------------------------------------------"
 
 # Build the project
 echo "Building the iOS project..."
