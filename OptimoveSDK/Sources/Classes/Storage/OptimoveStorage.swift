9a121c69be51c83449c9f965728c613f7d39a002
Eli Gutovsky <eli_g@optimove.com>
2023-12-08 14:14:39 +0200

    refactor: split storage facade to files

---
+++ OptimoveSDK/Sources/Classes/Storage/OptimoveStorage.swift
@@ -0,0 +1,4 @@
+//  Copyright © 2023 Optimove. All rights reserved.
+
+/// Combined protocol for a convenince access to stored values and files.
+typealias OptimoveStorage = FileStorage & KeyValueStorage & StorageValue
