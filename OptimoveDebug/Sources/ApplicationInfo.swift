334c4c4d1324d8073aa1b6db8d2084942cb6c92d
Eli Gutovsky <eli_g@optimove.com>
2024-02-06 13:25:25 +0200

    feat: expose sdk state

--- OptimoveDebug/Sources/ApplicationInfo.swift
+++
@@ -1,9 +0,0 @@
-//  Copyright © 2024 Optimove. All rights reserved.
-
-import Foundation
-
-public enum ApplicationInfo {
-    public static let bundleIdentifier = Bundle.main.bundleIdentifier!
-    public static let version = Bundle.main.infoDictionary!["CFBundleShortVersionString"] as! String
-    public static let build = Bundle.main.infoDictionary!["CFBundleVersion"] as! String
-}
