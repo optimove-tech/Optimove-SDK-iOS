0dcca5fe2b5069bfa56663c76ec8cf64b032b550
Daniela Todorova <daniela_t@optimove.com>
2024-08-06 13:50:38 +0100

    Add latest changes and improvements

--- OptimoveSDK/Sources/Classes/Optimobile/PreferenceCenter.swift
+++ OptimoveSDK/Sources/Classes/Optimobile/PreferenceCenter.swift
@@ -9,12 +9,12 @@ enum TenantIDError: Error {
 class PreferenceCenter {
     private var networkClient: NetworkClient
     private var storage: KeyValueStorage
-    
+
     init(storage: KeyValueStorage, networkClient: NetworkClient) {
         self.networkClient = networkClient
         self.storage = storage
     }
-    
+
     private func getTenantId() throws -> String {
         if let tenantIdValue = storage.value(for: .tenantID) as? Int {
             return String(tenantIdValue)
@@ -22,93 +22,64 @@ class PreferenceCenter {
             throw TenantIDError.conversionFailed
         }
     }
-    
-    //attempt 1
-    func getPrefs(_ completion: @escaping (Result<Preferences, Error>) -> Void) {
-        do {
-            let tenantId = try getTenantId()
-            
-            let request = try NetworkRequest(
-                method: .get,
-                baseURL: URL(string: "https://preference-center-dev-pb.optimove.net")!,
-                path: "/api/v1/preferences",
-                headers: [
-                    HTTPHeader(field: .accept, value: .textplain),
-                    HTTPHeader(field: .tenantId, value: tenantId)
-                ],
-                queryItems:[
-                    URLQueryItem(name: "customerId", value: "daniela-t"),
-                    URLQueryItem(name: "brandGroupId", value: "eba9b5c8-7d18-4912-9af1-f3f2f68f5d87")
-                ]
-            )
-            
-            networkClient.perform(request) { result in
-                completion(
-                    Result {
-                        let data = try result.get()
-                        let preferences = try data.decode(to: Preferences.self)
-                        return preferences
-                    }
-                )
-            }
-        } catch {
-            print("Failed to create network request: \(error)")
-            completion(.failure(error))
+
+    private func getCustomerId() throws -> String {
+        if let customerId = storage.value(for: .customerID) as? String {
+            return customerId
+        } else {
+            throw StorageError.noValue(.customerID)
         }
     }
-    
-    
-    //attempt 2 I think it's better to go with this
-    func getPrefsAsync(for customerId: String, brandGroupId: String) async throws -> Preferences {
+
+    func getPreferences(brandGroupId: String) async throws -> Preferences {
         do {
+            self.storage.set(value: "daniela-customer", key: .customerID)
+            self.storage.set(value: 3013, key: .tenantID)
+            let customerId = try getCustomerId()
             let tenantId = try getTenantId()
-            
+
             let request = try NetworkRequest(
                 method: .get,
                 baseURL: URL(string: "https://preference-center-dev-pb.optimove.net")!,
                 path: "/api/v1/preferences",
                 headers: [
                     HTTPHeader(field: .accept, value: .textplain),
-                    HTTPHeader(field: .tenantId, value: tenantId)
+                    HTTPHeader(field: .tenantId, value: .tenantId(id: tenantId))
                 ],
                 queryItems:[
                     URLQueryItem(name: "customerId", value: customerId),
                     URLQueryItem(name: "brandGroupId", value: brandGroupId)
                 ]
             )
-            
-            
-            let data = try await networkClient.performAsync(request)
-            let preferences = try JSONDecoder().decode(Preferences.self, from: data!)
+
+
+            let data: Data = try cast(await networkClient.performAsync(request))
+            let preferences = try JSONDecoder().decode(Preferences.self, from: data)
+
             return preferences
-   
-            //           await networkClient.perform(request) { result in
-            //                Result {
-            //                    let data = try result.get()
-            //                    let preferences = try data.decode(to: Preferences.self)
-            //                    print(preferences)
-            //                    return preferences
-            //                }
-            //            }
         }
         catch {
             print("Failed to create network request: \(error)")
         }
-        
+
         throw NetworkError.requestFailed
     }
-    
-    
-    func setPrefsAsync(for customerId: String, brandGroupId: String, updates preferenceUpdates : [PreferenceUpdate]) async throws -> Preferences {
+
+
+    func setPreferences(for customerId: String, brandGroupId: String, updates preferenceUpdates : [PreferenceUpdate]) async throws -> Preferences {
         do {
+            self.storage.set(value: "daniela-customer", key: .customerID)
+            self.storage.set(value: 3013, key: .tenantID)
+            let customerId = try getCustomerId()
             let tenantId = try getTenantId()
+
             let request = try NetworkRequest(
                 method: .put,
                 baseURL: URL(string: "https://preference-center-dev-pb.optimove.net")!,
                 path: "/api/v1/preferences",
                 headers: [
                     HTTPHeader(field: .accept, value: .textplain),
-                    HTTPHeader(field: .tenantId, value: tenantId)
+                    HTTPHeader(field: .tenantId, value: .tenantId(id: tenantId))
                 ],
                 queryItems:[
                     URLQueryItem(name: "customerId", value: customerId),
@@ -116,24 +87,17 @@ class PreferenceCenter {
                 ],
                 body: preferenceUpdates
             )
-            
-            
-            await networkClient.perform(request) { result in
-                Result {
-                    print(result)
-                    let data = try result.get()
-                    let preferences = try data.decode(to: Preferences.self)
-                    print(preferences)
-                    return preferences
-                }
-            }
+
+            let data: Data = try cast(await networkClient.performAsync(request))
+            let preferences = try JSONDecoder().decode(Preferences.self, from: data)
+
+            return preferences
         }
         catch {
             print("Failed to create network request: \(error)")
+            throw NetworkError.requestFailed
         }
-        
-        throw NetworkError.requestFailed
     }
-    
+
 }
 
--- OptimoveSDK/Sources/Classes/Optimobile/PreferenceCenterModels.swift
+++ OptimoveSDK/Sources/Classes/Optimobile/PreferenceCenterModels.swift
@@ -6,25 +6,86 @@ struct PreferenceCenterConfig: Codable {
     var brandGroupId: String
 }
 
-public struct Preferences: Codable {
+public class Preferences: NSObject, Codable {
     var topics: [Topic]
     var channels: [Channel]
+    
+    public func getChannels() -> [Channel] {
+        return channels
+    }  
+    
+    public func getTopics() -> [Topic] {
+        return topics
+    }
+    
+    public init(topics: [Topic], channels: [Channel]) {
+        self.topics = topics
+        self.channels = channels
+    }
+    
+   required public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.topics = try container.decode([Topic].self, forKey: .topics)
+        self.channels = try container.decode([Channel].self, forKey: .channels)
+    }
 }
 
-enum Channel: Int, Codable {
+public enum Channel: Int, Codable {
     case mobilePush = 489
     case webPush = 490
     case sms = 493
 }
 
-struct Topic: Codable {
-    var topicId: String
+public class Topic: NSObject, Codable {
+   public var topicId: String
     var topicName: String
     var topicDescription: String
     var channelSubscription: [Channel]
+    
+    public func getId() -> String {
+        return topicId
+    }
+    
+    public func getName() -> String {
+        return topicName
+    }
+    
+    public func getDescription() -> String {
+        return topicDescription
+    }
+    
+    public func getSubscribedChannels() -> [Channel] {
+        return channelSubscription
+    }
+    
+    public init(id: String, name: String, description: String, subscribedChannels: [Channel]) {
+        topicId = id
+        topicName = name
+        topicDescription = description
+        channelSubscription = subscribedChannels
+    }
+    
+    public required init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.topicId = try container.decode(String.self, forKey: .topicId)
+        self.topicName = try container.decode(String.self, forKey: .topicName)
+        self.topicDescription = try container.decode(String.self, forKey: .topicDescription)
+        self.channelSubscription = try container.decode([Channel].self, forKey: .channelSubscription)
+    }
 }
 
-public struct PreferenceUpdate: Codable {
+public class PreferenceUpdate: NSObject, Codable {
     var topicId: String
-    var subscribedChannels: [Channel]
+    var channelSubscription: [Channel]
+    
+    public init(topicId: String, channelSubscription: [Channel]) {
+        self.topicId = topicId
+        self.channelSubscription = channelSubscription
+    }
+    
+    required public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.topicId = try container.decode(String.self, forKey: .topicId)
+        self.channelSubscription = try container.decode([Channel].self, forKey: .channelSubscription)
+    }
 }
