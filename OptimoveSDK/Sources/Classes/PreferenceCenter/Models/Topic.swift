c9c4468baa09a46a2acbcea699565b1a44d342d6
Daniela Todorova <daniela_t@optimove.com>
2024-08-08 16:45:11 +0100

    Remove getters

--- OptimoveSDK/Sources/Classes/PreferenceCenter/Models/Topic.swift
+++ OptimoveSDK/Sources/Classes/PreferenceCenter/Models/Topic.swift
@@ -1,39 +1,15 @@
 import Foundation
 
 public class Topic: NSObject, Codable {
-    public var topicId: String
-    var topicName: String
-    var topicDescription: String
-    var channelSubscription: [Channel]
-
-    public func getId() -> String {
-        return topicId
-    }
-
-    public func getName() -> String {
-        return topicName
-    }
-
-    public func getDescription() -> String {
-        return topicDescription
-    }
-
-    public func getSubscribedChannels() -> [Channel] {
-        return channelSubscription
-    }
+    private(set) var topicId: String
+    private(set) var topicName: String
+    private(set) var topicDescription: String
+    private(set) var channelSubscription: [Channel]
 
     public init(id: String, name: String, description: String, subscribedChannels: [Channel]) {
         topicId = id
         topicName = name
         topicDescription = description
         channelSubscription = subscribedChannels
-    }
-
-    public required init(from decoder: any Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.topicId = try container.decode(String.self, forKey: .topicId)
-        self.topicName = try container.decode(String.self, forKey: .topicName)
-        self.topicDescription = try container.decode(String.self, forKey: .topicDescription)
-        self.channelSubscription = try container.decode([Channel].self, forKey: .channelSubscription)
     }
 }
