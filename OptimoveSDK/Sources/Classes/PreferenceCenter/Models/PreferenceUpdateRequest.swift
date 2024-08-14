f0126d23fad3723edc6ee806ca3b6558167e08af
Daniela Todorova <daniela_t@optimove.com>
2024-08-14 11:14:27 +0100

    Take enum out of initializer, oops

--- OptimoveSDK/Sources/Classes/PreferenceCenter/Models/PreferenceUpdateRequest.swift
+++ OptimoveSDK/Sources/Classes/PreferenceCenter/Models/PreferenceUpdateRequest.swift
@@ -4,13 +4,13 @@ public struct PreferenceUpdateRequest: Codable {
     public let topicId: String
     public let subscribedChannels: [Channel]
 
+    enum CodingKeys: String, CodingKey {
+        case topicId = "topicId"
+        case subscribedChannels = "channelSubscription"
+    }
+
     public init(topicId: String, subscribedChannels: [Channel]) {
         self.topicId = topicId
         self.subscribedChannels = subscribedChannels
-
-        enum CodingKeys: String, CodingKey {
-            case topicId = "topicId"
-            case subscribedChannels = "channelSubscription"
-        }
     }
 }
